ff9281438f1e6178c5c45b63d9d5f050e6b7e866
mfuku <daisycodes@gmail.com>
2021-04-12 21:32:58 +0900

    init

---
+++ pages/_year/_month/_day/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <h1>{{post.title}}</h1>
+    <p>{{post.date}}</p>
+    <nuxt-content :document="post" />
+  </article>
+</template>
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const post = await $content('posts',params.year,params.month, params.slug || 'index').fetch()
+    // const post = await $content('posts', {deep: true}).fetch()
+    return { post }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="b in posts" :key="b.slug">
+      <nuxt-link :to="'/post/'+ b.slug">{{b.title}} {{b.date}}</nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const query = await $content('posts' || 'index').limit(10)
+    const posts = await query.fetch()
+    return { posts }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
