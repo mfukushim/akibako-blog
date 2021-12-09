2d3963cbfd2f8652dc0c22bdaf41b8bd6975d061
mfuku <daisycodes@gmail.com>
2021-12-10 00:03:00 +0900

    fix type mismatch

--- services/Common.ts
+++ services/Common.ts
@@ -1,5 +1,5 @@
 import jp from 'jsonpath'
-import { IContentDocument } from '@nuxt/content/types/content'
+import {IContentDocumentBase} from '@nuxt/content/types/content'
 
 export interface BlogInfo {
   title: string;
@@ -8,32 +8,32 @@ export interface BlogInfo {
   categories: string[];
   //
   open: boolean;
-  summary:string;
+  summary: string;
   //
   post: any,
   image?: string;
   //
   year: string;
-  month:string;
-  day:string;
+  month: string;
+  day: string;
   link: string;
   cid?: string;
 }
 
 export class Common {
-  public static getPostList (posts: IContentDocument | IContentDocument[]):BlogInfo[] {
+  public static getPostList(posts: IContentDocumentBase | IContentDocumentBase[]): BlogInfo[] {
     const reg = /\/(.+)\/(\d{4})-(\d{2})-(\d{2})-(.+)\/index/
     return posts.reduce((p: BlogInfo[], c: any) => {
       const m = c.path.match(reg)
       if (m) {
         const pick = Common.pickBody(c)
         p.push({
-            year: m[2],
-            month: m[3],
-            day: m[4],
-            link: m[5],
-            slug: `/${m[2]}/${m[3]}/${m[4]}/${m[5]}`,
-            open: false,
+          year: m[2],
+          month: m[3],
+          day: m[4],
+          link: m[5],
+          slug: `/${m[2]}/${m[3]}/${m[4]}/${m[5]}`,
+          open: false,
           ...pick
         })
         // const pick = jp.query(c, '$.body..children[?(@.type=="text")]') as { value: string }[]
@@ -58,7 +58,7 @@ export class Common {
     }, [])
   }
 
-  public static pickBody(post:any): { date: string; summary: string; image?: string; post: any; categories: string[]; title: string; cid: string } {
+  public static pickBody(post: any): { date: string; summary: string; image?: string; post: any; categories: string[]; title: string; cid: string } {
     const pick = jp.query(post, '$.body..children[?(@.type=="text")]') as { value: string }[]
     const img = jp.query(post, '$.body..children[?(@.type=="element" && @.tag=="img")]') as { props: { src: string } }[]
     return {
@@ -68,7 +68,7 @@ export class Common {
       categories: [],
       summary: (pick && pick.length > 0) ? pick.map(value => value.value.trim()).join('').substring(0, 50) + '...' : '',
       image: (img && img.length > 0) ? img[0].props.src : undefined,
-      post: post
+      post
     }
   }
 }
